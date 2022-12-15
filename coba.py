69b6497c3beeefa2658b250dcb22b78558d97d59
Ulinnuh4 <ulinnuharusharyadi@gmail.com>
2022-12-15 13:53:06 +0700

    menambahkan progam dan foto

---
+++ coba.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+def gambar_mercury():
+    with Image.open("mercury.jpeg") as im:
+        im.show()
+def gambar_venus():
+    with Image.open("venus.jpeg") as im:
+        im.show()
+def gambar_bumi():
+    with Image.open("bumi.jpeg") as im:
+        im.show()
+def gambar_mars():
+    with Image.open("mars.jpeg") as im:
+        im.show()
+def gambar_jupiter():
+    with Image.open("jupiter.jpeg") as im:
+        im.show()
+def gambar_saturnus():
+    with Image.open("saturnus.jpeg") as im:
+        im.show()
+def gambar_uranus():
+    with Image.open("uranus.jpeg") as im:
+        im.show()
+def gambar_neptunus():
+    with Image.open("neptunus.jpeg") as im:
+        im.show()
+    
+
+
+        
+
