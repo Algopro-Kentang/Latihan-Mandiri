6826a4bb9b067bf6db23b367049167f169ed786e
Ulinnuh4 <ulinnuharusharyadi@gmail.com>
2022-12-15 13:57:31 +0700

    menambahkan progam dan foto

---
+++ Menampilkan_Planet.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+def gambar_mercury():
+    with Image.open("Gambar_Planet/mercury.jpeg") as im:
+        im.show()
+def gambar_venus():
+    with Image.open("Gambar_Planet/venus.jpeg") as im:
+        im.show()
+def gambar_bumi():
+    with Image.open("Gambar_Planet/bumi.jpeg") as im:
+        im.show()
+def gambar_mars():
+    with Image.open("Gambar_Planet/mars.jpeg") as im:
+        im.show()
+def gambar_jupiter():
+    with Image.open("Gambar_Planet/jupiter.jpeg") as im:
+        im.show()
+def gambar_saturnus():
+    with Image.open("Gambar_Planet/saturnus.jpeg") as im:
+        im.show()
+def gambar_uranus():
+    with Image.open("Gambar_Planet/uranus.jpeg") as im:
+        im.show()
+def gambar_neptunus():
+    with Image.open("Gambar_Planet/neptunus.jpeg") as im:
+        im.show()
+    
+
+
+        
+
