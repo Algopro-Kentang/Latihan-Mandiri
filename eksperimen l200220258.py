01f59f586ff7008f094a4496b0541ed8e9933cac
Ikhlasul Rafid <120313599+Mahezzz@users.noreply.github.com>
2022-12-26 19:12:11 +0700

    Add files via upload

---
+++ eksperimen l200220258.py
@@ -0,0 +1,27 @@
+
+# fungsi
+def hargabesi():
+    print('harga besi adalah 25.000')
+def hargakayu():
+    print('harga kayu adalah 50.000')
+def hargabata():
+    print('harga bata adalah 2.000')
+
+# input
+def hargatotalbesi(pcs):
+    harga = 25000
+    hargatotal = pcs*harga
+    print('harga',pcs,'besi adalah',hargatotal)
+
+def hargatotalkayu(pcs):
+    harga = 50000
+    hargatotal = pcs*harga
+    print('harga',pcs,'besi adalah',hargatotal)
+
+def hargatotalbata(pcs):
+    harga = 2000
+    hargatotal = pcs*harga
+    print('harga',pcs,'bata adalah',hargatotal)
+
+(hargatotalbata(3))
+hargakayu()
